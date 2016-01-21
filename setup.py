c8aadc0c8bd7c0c9f3e68d8f02a09ca2eb51a844
Adam Vessey <adam-vessey@users.noreply.github.com>
2016-01-21 14:47:05 +0000

    More PEP-8.

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ Setup script for dgi_repo.
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='dgi_repo',
+setup(
+    name='dgi_repo',
     version='1.x',
     description="discoverygarden's repository software",
     author='discoverygarden',
     author_email='dev@discoverygarden.ca',
     license='TBD',
     packages=find_packages(),
-    package_dir={'dgi_repo':'dgi_repo'},
+    package_dir={'dgi_repo': 'dgi_repo'},
     package_data={'dgi_repo': ['resources/*.*']},
     long_description=read('README.md'),
     install_requires=[
