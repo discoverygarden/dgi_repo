edda861465898ba7f5b6294fbc50ccd38ac71ce9
Adam Vessey <adam-vessey@users.noreply.github.com>
2016-01-21 13:58:01 +0000

    Missed some quotes...

--- dgi_repo/auth/drupal.py
+++ dgi_repo/auth/drupal.py
@@ -1,4 +1,4 @@
-'''
+"""
 Authentication helpers.
 
 Will need some means of configuring Drupal endpoints, likely as a dictionary of
@@ -6,12 +6,12 @@ dictionaries, mapping our site-identifying keys to maps of DB creds and some
 key identifying the DB driver to use... And possibly the query (could we have a
 default?)... Or materialization directly out of YAML
 (http://pyyaml.org/wiki/PyYAMLDocumentation#YAMLtagsandPythontypes)?
-'''
+"""
 import talons.auth.basicauth
 
 
 def authenticate(identity):
-    '''
+    """
     Check if the given identity is valid, and set the relevant roles.
 
     Likely used with talons.auth.external.Authenticator.
@@ -21,7 +21,7 @@ def authenticate(identity):
 
     Returns:
         A boolean indicating if the given identity authenticates.
-    '''
+    """
     # TODO: Grab the config for the selected site (default if none indicated?)
     # TODO: Get a DB cursor for the selected site.
     # TODO: Check the credentials against the selected site (using provided
@@ -30,13 +30,13 @@ def authenticate(identity):
 
 
 class SiteBasicIdentifier(talons.auth.basicauth.Identifier):
-    '''
+    """
     Determine against which site to validate credentials.
-    '''
+    """
     def identify(self, req):
-        '''
+        """
         Look at the headers in the request for our identifying header.
-        '''
+        """
         result = super().identify(req)
 
         if result:
