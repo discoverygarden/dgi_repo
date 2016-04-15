b0f4cb65e4da1101a6ca4cf23a8e3f383317106a
willtp87 <willtp87@hotmail.com>
2016-04-15 11:09:54 -0300

    Not printing exception msg twice.

--- dgi_repo/fcrepo3/exceptions.py
+++ dgi_repo/fcrepo3/exceptions.py
@@ -26,7 +26,7 @@ def handle_exception(ex, req, resp, params):
     Custom Falcon exception handler that ensures we send 500s.
     """
     if not isinstance(ex, falcon.HTTPError):
-        logger.exception(ex)
+        logger.exception('Uncaught exception:')
         raise falcon.HTTPError('500 Internal Server Error') from ex
     else:
         raise ex
