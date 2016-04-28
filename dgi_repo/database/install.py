c35b21c433cdd09d3854b9a275c61a45a5b227ba
willtp87 <willtp87@hotmail.com>
2016-04-28 11:45:20 -0300

    will need to add object data to install

--- dgi_repo/database/install.py
+++ dgi_repo/database/install.py
@@ -5,6 +5,8 @@ Install related DB functions.
 import logging
 from os.path import join, dirname
 
+from psycopg2.extensions import ISOLATION_LEVEL_REPEATABLE_READ
+
 import dgi_repo.fcrepo3.relations as rels
 from dgi_repo.database.utilities import get_connection
 
@@ -19,8 +21,8 @@ def install_schema():
     with db_connection:
         sql_file_path = join(dirname(__file__), 'resources', 'dgi_repo.sql')
         with open(sql_file_path, 'r') as schema_file:
-            with db_connection.cursor() as db_cursor:
-                db_cursor.execute(schema_file.read())
+            with db_connection.cursor() as cursor:
+                cursor.execute(schema_file.read())
     db_connection.close()
     logger.info('Installed schema.')
 
@@ -31,19 +33,21 @@ def install_base_data():
     """
     import dgi_repo.database.write.relations as relations_writer
 
-    db_connection = get_connection()
-    with db_connection:
-        with db_connection.cursor() as cursor:
-            for namespace, predicates in rels.RELATIONS.items():
+    db_connection = get_connection(
+        isolation_level=ISOLATION_LEVEL_REPEATABLE_READ
+    )
+    with db_connection, db_connection.cursor() as cursor:
+        for namespace, predicates in rels.RELATIONS.items():
+            # Default relationship data.
+            relations_writer.upsert_namespace(namespace, cursor=cursor)
+            namespace_id, = cursor.fetchone()
+            for predicate in predicates:
+                relations_writer.upsert_predicate(
+                    {'namespace': namespace_id, 'predicate': predicate},
+                    cursor=cursor
+                )
 
-                relations_writer.upsert_namespace(namespace, cursor)
-                namespace_id, = cursor.fetchone()
-
-                for predicate in predicates:
-                    relations_writer.upsert_predicate(
-                        {'namespace': namespace_id, 'predicate': predicate},
-                        cursor
-                    )
+            # @TODO: default object data.
 
     db_connection.close()
     logger.info('Installed base data.')
